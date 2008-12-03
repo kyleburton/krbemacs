8a55db12c6a666cb323a8ef7b04360ed39fcb337
Kyle R. Burton <mortis@kburton-lin.ws.hmsonline.com>
2008-12-03 17:32:54 -0500

    signature parsing

--- lib/krb-java.el
+++ lib/krb-java.el
@@ -116,7 +116,7 @@
  (equalp '("<String, Map<String,String>>" " foof")  (krbj-parse-delimited "<String, Map<String,String>> foof" "<" ">")))
 
 
-(defun krbj-pull-type (signature)
+(defun krbj-pull-return-type (signature)
   (cond ((string-match "^[ \t]*\\([^< \t]+\\)[ \t]+[a-zA-Z]" signature)
          (list
           (match-string 1 signature)
@@ -130,34 +130,135 @@
              (list (concat base-type generic-type)
                    sub-remainder))))
         (t
-         (error "don't know how to parse: '%s'" signature))))
+         (error "krbj-pull-return-type: don't know how to parse: '%s'" signature))))
 
 (krbj-run-tests
- (equalp "void"     (first (krbj-pull-type "void main(String...args)")))
- (equalp "String"   (first (krbj-pull-type "String foo(String...args)")))
- (equalp "Map<String,Map<String,String>>"   (first (krbj-pull-type "Map<String,Map<String,String>> foo(String...args)")))
- (equalp "Map<String,Map<String,String>>"   (first (krbj-pull-type "Map<String,Map<String,String>> blah, String foof"))))
+ (equalp "void"     (first (krbj-pull-return-type "void main(String...args)")))
+ (equalp "String"   (first (krbj-pull-return-type "String foo(String...args)")))
+ (equalp "Map<String,Map<String,String>>"   (first (krbj-pull-return-type "Map<String,Map<String,String>> foo(String...args)")))
+ (equalp "Map<String,Map<String,String>>"   (first (krbj-pull-return-type "Map<String,Map<String,String>> blah, String foof"))))
+
+(defun krbj-pull-method-name (signature)
+  (cond ((string-match "^[ \t]*\\([a-zA-Z_][a-zA-Z_0-9]*\\)[ \t]*(" signature)
+         (list
+          (match-string 1 signature)
+          (substring signature (match-end 1))))
+        (t
+         (error "Can't parse method-name from '%s'" signature))))
+
+(krbj-run-tests
+ (equalp '("foo" "(String bar)") (krbj-pull-method-name "foo(String bar)"))
+ (equalp '("foo" "(String bar)") (krbj-pull-method-name " foo(String bar)"))
+ (equalp '("_99" " (int ii) {")  (krbj-pull-method-name "_99 (int ii) {")))
+
+(defun krbj-parse-throws (signature)
+  (cond ((string-match "^[ \t]*throws[ \t\n]+\\(.+?\\)[ \t\n]*{" signature)
+         (list 
+          (match-string 1 signature)
+          (substring signature (match-end 1))))
+        (t
+         (list "" signature))))
+
+(krbj-run-tests
+ (equalp '("" " { ... }")
+         (krbj-parse-throws " { ... }"))
+ (equalp '("IOException" " { ... }")
+         (krbj-parse-throws " throws IOException { ... }"))
+ (equalp '("IOException, java.io.FileNotFoundException, NullPointerException" " { ... }")
+         (krbj-parse-throws " throws IOException, java.io.FileNotFoundException, NullPointerException { ... }")))
+
+
+(defun krbj-pull-type (signature)
+  (setf signature  (replace-regexp-in-string "^[ \t,]+" "" signature))
+  (cond ((string-match "^[ \t]*\\([a-zA-Z_0-9\.]+\\)\\(\\.\\.\\.\\|[ 	]+\\)" signature)
+         (message "matched: '%s' '%s'" 
+                  (match-string 1 signature)
+                  (match-string 2 signature))
+         (if (string= "..."
+                      (match-string 2 signature))
+             (list (concat (match-string 1 signature)
+                           (match-string 2 signature))
+                   (substring signature (match-end 2)))
+           (list 
+            (match-string 1 signature)
+            (substring signature (match-end 1)))))
+        ((string-match "^[ \t]*\\([^< \t]+\\)[ \t]*<" signature)
+         (let* ((base-type    (match-string 1 signature))
+                (remainder    (substring signature (match-end 1))))
+           (destructuring-bind
+               (generic-type sub-remainder)
+               (krbj-parse-delimited remainder "<" ">")
+             (list (concat base-type generic-type)
+                   sub-remainder))))
+        (t
+         (error "krbj-pull-type: don't know how to parse: '%s'" signature))))
+
+(krbj-run-tests
+ (equalp '("String..." "args")         (krbj-pull-type "String...args"))
+ (equalp '("int" " ii, String...args") (krbj-pull-type "int ii, String...args")))
+
+(defun krbj-pull-variable (signature)
+  (cond ((string-match "^[ \t]*\\([a-zA-Z_][a-zA-Z_0-9]*\\)" signature)
+         (list
+          (match-string 1 signature)
+          (substring signature (match-end 1))))
+        (t
+         (error "Can't parse method-name from '%s'" signature))))
+
+(krbj-run-tests
+ (equalp '("ii" ", String...args") (krbj-pull-variable " ii, String...args")))
+
+(defun krbj-parse-args-list (signature)
+  ;; assert that it is enclosed in ()'s
+  (when  (string-match "(\\([^)]+\\))" signature)
+    (setf signature (match-string 1 signature)))
+  (let ((res (list)))
+    (loop for ii from 0 upto 5
+          while (not (= 0 (length signature)))
+          with tmp = nil
+          with type = nil
+          do
+          (setf tmp (krbj-pull-type signature))
+          (setf type (first tmp))
+          (setf signature (second tmp))
+          (setf tmp (krbj-pull-variable signature))
+          (push (list type (first tmp))
+                res)
+          (setf signature (second tmp))
+          (message "at[%s]: %s" ii signature))
+    (reverse res)))
+
+(krbj-parse-args-list "(int ii, java.util.Map<String,Map<String,Integer>> things, java.lang.String...args)")
 
 (defun krbj-parse-method-signature (signature)
   (let ((res (list))
-        (tmp nil))
+        (tmp nil)
+        (args nil))
     ;; pull the protection
-    (setf tmp (krbj-pull-type signature))
+    (setf tmp (krbj-pull-protection signature))
     (push (list 'protection (first tmp)) res)
     (setf signature (second tmp))
     ;; pull the return type
-    (setf tmp (krbj-pull-type signature))
-    (push (list 'return (first tmp))
-          res)
+    (setf tmp (krbj-pull-return-type signature))
+    (push (list 'return (first tmp)) res)
     (setf signature (second tmp))
     ;; pull the method-name
+    (setf args (krbj-pull-method-name signature))
+    (push (list 'name (first args)) res)
+    (setf signature (second args))
     ;; grab the args list
-    ;;   loop while there are args left
-    ;;     pull a type
+    (setf tmp (krbj-parse-delimited signature "(" ")"))
+    (push (list 'args-as-string (first tmp)) res)
+    (setf signature (second tmp))
+    ;; parse the args list
+    (push (list 'args (krbj-parse-args-list (first tmp))) res)
     ;; look for throws clause
+    (setf tmp (krbj-parse-throws signature))
+    (push (list 'throws-as-string (first tmp)) res)
+    (setf signature (second tmp))
     (reverse res)))
 
-(krbj-parse-method-signature "public Map<String, Map<String,Integer>> someMethod(String..args) throws IOException {")
+(krbj-parse-method-signature "public Map<String, Map<String,Integer>> someMethod(int ii, java.util.Map<String,Map<Integer,String>> things, String...args) throws IOException {")
 
 (defun krbj-elisp-tests ()
   (assert
