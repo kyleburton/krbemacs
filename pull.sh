c36dd8bcbff94b913e58bed6db4f84443766f683
Kyle Burton <mortis@indigo64.(none)>
2009-05-31 22:29:03 -0400

    unnecessary

--- pull.sh
+++
@@ -1,3 +0,0 @@
-#time rsync -rvh asymmetr@asymmetrical-view.com:public_html/personal/config/emacs/ ./
-#date
-git pull asymmetr@asymmetrical-view.com:public_html/personal/config/emacs/
--- push.sh
+++
@@ -1,2 +0,0 @@
-time rsync --exclude .svn -rvh ./ asymmetr@asymmetrical-view.com:public_html/personal/config/emacs
-date
