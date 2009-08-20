c08d3866022cce9fb2c2b87db035fd5d81103395
Kyle Burton <kyle.burton@algorithmics.com>
2009-08-19 21:05:06 -0400

    helpers for the workshop

---
+++ port-forward.sh
@@ -0,0 +1,22 @@
+LOCAL_PORT=$1
+REMOTE_PORT=$2
+HOST=$3
+
+function usage () {
+    echo "Please specify a port to forward: $0 local-port remote-port [host=localhost]"
+}
+
+if [ -z "$LOCAL_PORT" ]; then
+    usage
+    exit 1
+fi
+
+if [ -z "$REMOTE_PORT" ]; then
+    REMOTE_PORT=$LOCAL_PORT
+fi
+
+if [ -z "$HOST" ]; then
+    HOST=localhost
+fi
+
+ssh -L$LOCAL_PORT:localhost:$REMOTE_PORT $HOST
---
+++ run-swank.clj
@@ -0,0 +1,5 @@
+(require 'swank.swank)
+(swank.swank/ignore-protocol-version "2009-08-19")
+(swank.swank/start-server "/var/folders/S4/S4BD+VyHGOqzYTY+piBsbU+++TI/-Tmp-/slime.krb" 
+                          :encoding "iso-latin-1-unix"
+                          :port 4005)
