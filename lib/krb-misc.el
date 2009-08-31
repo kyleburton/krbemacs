877f68613ee32de507dfb022badb8121105812f7
Kyle Burton <kyle.burton@algorithmics.com>
2009-08-30 23:11:37 -0400

    helper functions for both:   - jump to/from spec for current buff   - execute ruby spec for current buff jump stack still needs work

--- lib/krb-misc.el
+++ lib/krb-misc.el
@@ -192,26 +192,75 @@ buffer and places the cursor at that position."
   "Locate the first directory, going up in the directory hierarchy, where we find a pom.xml file - this will be a suitable place from which to execute the maven (mvn) command."
   (krb-find-containing-parent-directory-of-current-buffer "pom.xml" start-dir))
 
+(defun krb-clear-buffer (buffer-name)
+  (interactive "bBuffer: ")
+  (if (get-buffer buffer-name)
+      (save-excursion
+        (set-buffer buffer-name)
+        (end-of-buffer)
+        (kill-region 1 (point)))))
+
+(defun krb-ensure-buffer-exists (buffer-name)
+  (if (not (get-buffer buffer-name))
+      (save-excursion
+       (switch-to-buffer buffer-name))))
+
+(defun krb-ins-into-buffer (buffer-name text)
+  (krb-ensure-buffer-exists buffer-name)
+  (save-excursion
+    (set-buffer buffer-name)
+    (insert text)))
+
+(defun krb-insf-into-buffer (buffer-name &rest args)
+  (krb-ins-into-buffer buffer-name (apply 'message args)))
+
+(defmacro krb-with-fresh-output-buffer (buffer-name &rest body)
+  `(let ((*krb-buffer-name* ,buffer-name)
+         ;; prevents it from additionally being displayed in a minibuffer when the output is small
+         (max-mini-window-height 0))
+     (krb-clear-buffer ,buffer-name)
+     ,@body
+     (save-excursion
+       (pop-to-buffer ,buffer-name))))
+
 (defun krb-java-exec-mvn (&optional mvn-options)
   (interactive)
-  (shell-command (format "echo %s; cd %s; mvn %s test"
-                         (krb-java-find-mvn-proj-root-dir)
-                         (krb-java-find-mvn-proj-root-dir)
-                         (or mvn-options ""))
-                 "*maven-output*"))
+  (let ((cmd (format "echo %s; cd %s; mvn %s test"
+                     (krb-java-find-mvn-proj-root-dir)
+                     (krb-java-find-mvn-proj-root-dir)
+                     (or mvn-options ""))))
+    (krb-with-fresh-output-buffer 
+     "*maven-output*"
+     (krb-insf-into-buffer "*maven-output*" "Executing: %s\n" cmd)
+     (shell-command "*maven-output*"))))
 
 (defvar *krb-ruby-ruby-location* "ruby")
 
 (defun krb-ruby-find-proj-root-dir (&optional start-dir)
     (krb-find-containing-parent-directory-of-current-buffer "Rakefile" start-dir))
 
-(defun krb-ruby-exec-rake (&optional rake-options)
+(defun krb-ruby-in-spec-file? ()
+  "Simply tests if the current buffer file name ends in '_spec.rb'"
+  (string-match "_spec\\.rb$" (buffer-file-name)))
+
+(defun krb-ruby-in-ruby-file? ()
+  "Simply tests if the current buffer file name ends in '.rb'"
+  (string-match "\\.rb$" (buffer-file-name)))
+
+(defun krb-tmp ()
   (interactive)
-  (shell-command (format "echo %s; cd %s; rake %s spec"
-                         (krb-ruby-find-proj-root-dir)
-                         (krb-ruby-find-proj-root-dir)
-                         (or rake-options ""))
-                 "*rake-output*"))
+  (message "krb-ruby-in-ruby-file? %s" (krb-ruby-in-ruby-file?)))
+
+(defun krb-ruby-exec-rake-spec (&optional rake-options)
+  (interactive)
+  (let ((cmd (format "echo %s; cd %s; rake %s spec"
+                     (krb-ruby-find-proj-root-dir)
+                     (krb-ruby-find-proj-root-dir)
+                     (or rake-options ""))))
+    (krb-with-fresh-output-buffer 
+     "*rake-output*"
+     (krb-insf-into-buffer "*rake-output*" "Executing: %s\n" cmd)
+     (shell-command cmd "*rake-output*"))))
 
 (defun krb-ruby-calculate-spec-name (&optional file-name)
   "Returns the spec file name for the current buffer by default
@@ -238,18 +287,43 @@ correctly.  The rakefile is located via
     (concat proj-root
      (replace-regexp-in-string ".rb$" "_spec.rb" file-path-within-project))))
 
+(defun krb-ruby-calculate-base-name-for-spec-buffer (&optional file-name)
+  "See `krb-ruby-calculate-spec-name'."
+  (let* ((file-name (or file-name buffer-file-name))
+         (proj-root (krb-ruby-find-proj-root-dir))
+         (file-path-within-project (replace-regexp-in-string
+                                    "^[^/]+" "app"
+                                    (substring file-name (length proj-root)))))
+    (concat proj-root
+     (replace-regexp-in-string "_spec.rb$" ".rb" file-path-within-project))))
+
+(defun krb-ruby-find-spec-file ()
+  "If in a spec file, attempts to open it's corresponding implementation file (.../spec/a/b/c.rb => .../app/a/b/c.rb)"
+  (interactive)
+  (if (krb-ruby-in-spec-file?)
+      (find-file (krb-ruby-calculate-base-name-for-spec-buffer))
+    (find-file (krb-ruby-calculate-spec-name))))
+
 (defun krb-ruby-ruby-location ()
   "This is here to support being overridden"
   (or *krb-ruby-ruby-location* "ruby"))
 
 (defun krb-ruby-exec-spec-for-buffer (&optional rake-options)
   (interactive)
-  (shell-command (format "cd %s; %s %s %s"
-                         (krb-ruby-find-proj-root-dir)
-                         (krb-ruby-ruby-location)
-                         (krb-ruby-calculate-spec-name)
-                         (or rake-options ""))
-                 "*rake-output*"))
+  (let* ((spec-file-name (if (krb-ruby-in-spec-file?)
+                             (buffer-file-name)
+                           (krb-ruby-calculate-spec-name)))
+         (cmd (format "cd %s; %s %s %s"
+                      (krb-ruby-find-proj-root-dir)
+                      (krb-ruby-ruby-location)
+                      spec-file-name
+                      (or rake-options ""))))
+    (krb-with-fresh-output-buffer 
+     "*rake-output*"
+     (krb-insf-into-buffer "*rake-output*" "Executing: %s\n" cmd)
+     (shell-command cmd "*rake-output*")
+     (set-buffer "*rake-output*")
+     (local-set-key "\C-cr" krb-ruby-output-mode-prefix-map))))
 
 (defun krb-tmp ()
   (interactive)
@@ -261,24 +335,90 @@ The project's root directory will be found by looking backwards up the file hier
 .git directory is found."
   (interactive)
   ;; TODO: emit the output into a special buffer with an interactive mode...
-  (let ((project-home (krb-find-containing-parent-directory-of-current-buffer ".git")))
-    (shell-command (format "cd '%s'; git grep '%s'"
-                           project-home
-                           search-term)
-                   nil ;; output buffer
-                   nil ;; error buffer
-                   )))
+  (let* ((project-home (krb-find-containing-parent-directory-of-current-buffer ".git"))
+         (cmd (format "cd '%s'; git grep '%s'"
+                      project-home
+                      search-term)))
+    (krb-with-fresh-output-buffer
+     "*git-output*"
+     (krb-insf-into-buffer "*git-output*" "Executing: %s\n" cmd)
+     (shell-command cmd "*git-output*"))))
 
+(defun krb-get-current-line-in-buffer ()
+  (save-excursion
+    (beginning-of-line)
+    (let ((start (point)))
+      (end-of-line)
+      (buffer-substring start (point)))))
 
-;; ruby keybindings
-(defvar krb-ruby-prefix-map
+(defun krb-tmp ()
+  (interactive)
+  (message "curr:%s" (krb-get-current-line-in-buffer)))
+
+(defvar *krb-jump-stack* (list))
+
+(defun krb-jump-stack-push (fname lnum)
+  (let ((lnum (if (numberp lnum)
+                  lnum
+                (car (read-from-string lnum)))))
+    (setq *krb-jump-stack* 
+          (cons (list 
+                 (buffer-file-name)
+                 (line-number-at-pos))
+                *krb-jump-stack*))
+    (find-file fname)
+    (goto-line lnum)))
+
+(defun krb-jump-stack-pop ()
+  (interactive)
+  (let ((pair (pop *krb-jump-stack*)))
+    (find-file (first pair))
+    (goto-line (second pair))))
+
+(defun krb-jump-to-file ()
+  "In a buffer, take the current line, parse it as a file /
+line-number.  If a buffer is not open for the file open it.  If
+the window is not split, split the window first.  Visit the
+buffer in the other frame.  In the buffer for that file, send it
+to the given line number."
+  (interactive)
+  (let ((current-line (krb-get-current-line-in-buffer)))
+    (message "current-line: %s" current-line)
+    (cond ((string-match "^\\([^:]+\\):\\([0-9]+\\).+$" current-line)
+           (message "string-matched: %s" current-line)
+           (let ((fname (match-string 1 current-line))
+                 (lnum  (match-string 2 current-line)))
+             (krb-jump-stack-push fname lnum))))))
+
+; (string-match "^\\([^:]+\\):\\d\\+"  "/Users/kburton/development/algo_collateral_web/spec/controllers/antic_demand_margin_calls_controller_spec.rb:69:")
+
+(define-derived-mode krb-ruby-output-mode text-mode "KRB Ruby Output Mode"
+  "Kyle's Ruby Output Mode for interacting with the output of tools like Rake, test and spec."
+  (local-set-key "\C-cr." krb-ruby-jump-to-file-at-point))
+
+;; (define-derived-mode krb-ruby-mode ruby-mode "KRB Ruby Output Mode"
+;;   "Kyle's Ruby Extension Mode for developing with Rails."
+;;   (local-set-key "\C-crt" krb-ruby-exec-spec-for-buffer)
+;;   (local-set-key "\C-crT" krb-ruby-find-spec-file))
+
+(defvar krb-ruby-mode-prefix-map
   (let ((map (make-sparse-keymap)))
-    (define-key map "s" 'krb-ruby-exec-spec-for-buffer)
-    map)
-  "Keybinding prefix map which can be bound for common functions in krb-misc's ruby support.")
+    (define-key map "\C-s" 'krb-ruby-exec-rake-spec)
+    (define-key map "t" 'krb-ruby-exec-spec-for-buffer)
+    (define-key map "T" 'krb-ruby-find-spec-file)
+    (define-key map "," 'krb-jump-stack-pop)
+    map))
+
+(defvar krb-ruby-output-mode-prefix-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map "." 'krb-jump-to-file)
+    (define-key map "," 'krb-jump-stack-pop)
+    map))
+
+;; (local-set-key "\C-cr" krb-ruby-output-mode-prefix-map)
 
 (add-hook 'ruby-mode-hook
           '(lambda ()
-             (local-set-key "\C-cr" krb-ruby-prefix-map)))
+             (local-set-key "\C-cr" krb-ruby-mode-prefix-map)))
 
 (provide 'krb-misc)
