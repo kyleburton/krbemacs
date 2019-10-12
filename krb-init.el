4f535a38803d8e973d1e1815c71249f5fdd5167d
Kyle Burton <kyle.burton@gmail.com>
2019-10-12 12:12:58 -0700

    windmove bindings; yank and path helper functions

--- krb-init.el
+++ krb-init.el
@@ -102,23 +102,56 @@ There are two things you can do about this warning:
 
 ;; (krb-find-buffer-with-name-prefix "*ag search text:")
 
+(defun krb-dirname (path)
+  (file-name-directory (directory-file-name path)))
+
+;; (krb-dirname "/this/that/other.txt")
+;; (krb-dirname "/this/that/")
+;; (krb-dirname "/this/that")
+;; (krb-dirname "/this/")
+;; (krb-dirname "/this")
+;; (krb-dirname "/")
+(defun krb-find-file-up-from-dir (fname dname)
+  (let* ((path     dname)
+         (fullpath (concat path fname)))
+    (while (and
+            (not (file-exists-p (concat path fname)))
+            (not (string= "/" path)))
+      (setq path     (krb-dirname path))
+      (setq fullpath (concat path fname)))
+    (if (string= "/" path)
+        nil
+      path)))
+
+;; (krb-find-file-up-from-dir ".git" "/home/kyle/code/github.com/lawrencexia/ticket-webapp/public_html/tix/src/views/InventoryModal/")
+
+(defun krb-find-file-up-from-current-buffer (fname)
+  (interactive "sFile Name:")
+  (let ((res (krb-find-file-up-from-dir fname default-directory)))
+    (message "krb-find-file-up-from-dir: %s" res)
+    res))
+
+(defun krb-git-dir-for-current-buffer ()
+  (krb-find-file-up-from-dir ".git" default-directory))
+
 (require 'ag)
 (defun krb-ag-search-dwim-im-feeling-lucky ()
   (interactive)
-  (ag (ag/dwim-at-point) default-directory)
+  ;; (ag (ag/dwim-at-point) default-directory)
+  (ag (ag/dwim-at-point) (krb-git-dir-for-current-buffer))
   ;; TODO: can we close the just opened window who's name starts with '*ag search text:'?
   (next-error 1))
 
 (defun krb-ag-search-dwim ()
   (interactive)
-  (ag (ag/dwim-at-point) default-directory)
-  ;; TODO: can we close the just opened window who's name starts with '*ag search text:'?
+  ;; (ag (ag/dwim-at-point) default-directory)
+  (ag (ag/dwim-at-point) (krb-git-dir-for-current-buffer))
   (next-error 1))
 
 (defun krb-ag-search (term)
   (interactive (list (read-string "Term: " (ag/dwim-at-point))))
-  (ag term default-directory)
-  ;; TODO: can we close the just opened window who's name starts with '*ag search text:'?
+  ;; (ag term default-directory)
+  (ag term (krb-git-dir-for-current-buffer))
   (next-error 1))
 
 (defun krb-next-error ()
@@ -203,6 +236,43 @@ There are two things you can do about this warning:
 
 (add-hook 'slime-mode-hook             #'krb-slime-mode-hook)
 
+(defun krb-yank-inner-region-delimited-by (delimiter)
+  (interactive "sDelimiter: ")
+  (let* ((delims '(("(" ")")
+                   (")" "(")
+                   ("{" "}")
+                   ("}" "{")
+                   ("[" "]")
+                   ("]" "[")))
+         (pair   (or (assoc delimiter delims)
+                     (list delimiter delimiter)))
+         (dopen  (car pair))
+         (dclose (cadr pair)))
+    (save-excursion
+      (search-forward dclose)
+      (let ((endpos (point)))
+        ;; (search-backward dopen)
+        (backward-sexp 1)
+        (kill-ring-save (+ (point) (length dopen)) (- endpos (length dclose)))))))
+
+;; idea: what if we left a 'mark', a position where I'd like text to be inserted
+;; I'm going to go to antoher spot and make a selection, when I'm done I'd like to
+;; be transported back (even if across buffers) and that text to be injected into
+;; the spot I originally marked
+;; (defvar krb-yank-to-here-mark nil)
+
+(defun krb-yank-to-here ()
+  (interactive)
+  ;; (make-local-variable 'krb-yank-to-here-mark)
+  ;; (setq krb-yank-to-here-mark (list (buffer-name) (point) (mark)))
+  (bookmark-set "krb-yank-to-here-mark"))
+
+(defun krb-yank-to-here-commit ()
+  (interactive)
+  (bookmark-jump "krb-yank-to-here-mark")
+  (yank))
+
+
 (load "krb-clojure.el")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -226,7 +296,8 @@ There are two things you can do about this warning:
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (add-hook 'sh-mode-hook #'krb-bash-mode-hook)
-(add-hook 'sh-mode-hook 'flycheck-mode)
+;; (add-hook 'sh-mode-hook 'flycheck-mode)
+;; (remove-hook 'sh-mode-hook 'flycheck-mode)
 
 
 ;; TODO: do this only for the appropriate modes (eg: Erlang)
@@ -253,3 +324,16 @@ There are two things you can do about this warning:
 (add-hook 'write-file-hooks 'delete-trailing-whitespace nil t)
 ;; I no likey the menu bar, don't need it, bye bye
 (menu-bar-mode -1)
+
+;; windmove / windmove bindings
+(global-set-key "\C-xwh" #'windmove-left)
+(global-set-key "\C-xwj" #'windmove-down)
+(global-set-key "\C-xwk" #'windmove-up)
+(global-set-key "\C-xwl" #'windmove-right)
+(global-set-key "\C-cra=" #'align-regexp)
+(global-set-key "\C-cryi" #'krb-yank-inner-region-delimited-by)
+
+(global-set-key "\C-cryh" #'krb-yank-to-here)
+(global-set-key "\C-cry!" #'krb-yank-to-here-commit)
+;; TODO: krb-yank-there (takes current selection & yanks to the spot
+;; we marked, appending as we yank more and more things?
